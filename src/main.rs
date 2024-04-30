955693886f0c1e86fd24b780bf0a18078f9ee0cc
MaeinesenieaM <david.costa.gomes71@gmail.com>
2024-04-29 22:17:31 -0300

    Made a crude way to display text. ':)' VERSION!

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,57 @@
 //possible required code.
 //use fastrand;
-
 use std::fs;
+//use std::io;
 
 use sdl2::pixels::Color;
-//use sdl2::render::*;
+use sdl2::video::GLProfile;
+//use sdl2::render::Canvas;
+
+use sdl2::ttf;
+//use sdl2::gfx;
 
 use sdl2::event::Event;
 use sdl2::keyboard::*;
 //use sdl2::rect::*;
 
+
 fn main() {
     println!("The box shall open, once again...");
 
     let sdl2 = sdl2::init().unwrap();
-    let video_subsystem = sdl2.video().unwrap();
+    let video_subsystem = sdl2.video().unwrap(); 
+
+    let gl_attr = video_subsystem.gl_attr();
+    gl_attr.set_context_profile(GLProfile::Core);
+    gl_attr.set_context_version(3, 2);
 
     let window = video_subsystem.window("Box", 800, 600)
+        .opengl()        
         .position_centered()
         .build().unwrap();
-    let mut canvas = window.into_canvas()
+
+    let mut canvas = window.into_canvas()       
         .present_vsync()
         .build().unwrap();
-
-    let mut event_pump = sdl2.event_pump().unwrap();
+    let texture_creator = canvas.texture_creator();   
 
     match directory_verifier() {
         Ok (()) => {}
         Err (error) => { println! ("{:?}", error); }
     };
 
+    let ttf = ttf::init().unwrap();
+    let ttf_font = ttf.load_font("./src/main_assets/Fixedsys.ttf", 32).expect("COULD NOT FIND FILE!");
+
+    //creates the texture for text #TODO! Create a way to do this more intuitive.#
+    let text_texture = ttf_font.render(":)").solid(Color::RGB(150, 150, 165)).unwrap().as_texture(&texture_creator).unwrap();
+
+    let mut event_pump = sdl2.event_pump().unwrap();
     'running: loop {
+        
         canvas.set_draw_color(Color::RGB(20, 20, 20));
         canvas.clear();
+        let _ = canvas.copy(&text_texture, None, None);
 
         for event in event_pump.poll_iter() {
             match event {
