0b9986548a8c21a795c8e6b266dc79fd516e7cd5
DAVID COSTA GOMES <david.costa.gomes71@gmail.com>
2024-08-12 10:22:51 -0300

    Added tree.rs in Items.

---
+++ src/Items/tree.rs
@@ -0,0 +1,35 @@
+use sdl2::event::Event;
+//use sdl2::pixels::Color;
+use sdl2::keyboard::*;
+
+use thebox::{Display, Write};
+
+pub const NAME: &str = "Tree";
+pub const ID: u8 = 7;
+
+pub fn start(display: &mut Display, event_pump: &mut sdl2::EventPump, write: &mut Write) {
+    'repeat: loop {
+        display.canvas.clear();
+
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'repeat,
+                _ => {}
+            }
+        }
+
+        display.draw_text_centered(
+            &write,
+            400,
+            550,
+            "There will be procedural a tree in here.",
+            16,
+        );
+
+        display.canvas.present();
+    }
+}
--- src/items.rs
+++ src/items.rs
@@ -4,6 +4,7 @@ pub mod rays;
 pub mod textures;
 pub mod audio;
 pub mod clock;
+pub mod tree;
 
 use thebox::{Display, Write};
 
@@ -20,6 +21,7 @@ pub fn select_item(
         4 => textures::start(display, event_pump, write),
         5 => audio::start(display, event_pump, write),
         6 => clock::start(display, event_pump, write),
+        7 => tree::start(display, event_pump, write),
         _ => {
             println!("COULD NOT FIND ANYTHING!");
         }
@@ -34,6 +36,7 @@ pub fn name_item (choice: i32) -> &'static str {
         4 => textures::NAME,
         5 => audio::NAME,
         6 => clock::NAME,
+        7 => tree::NAME,
         _ => {
             "<NOTHING>"
         }
